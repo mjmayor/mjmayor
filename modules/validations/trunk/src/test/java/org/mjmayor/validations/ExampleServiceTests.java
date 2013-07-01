56b9f602b464c09f9de20bda6a45546deb88bb22
mjmp.84@gmail.com <mjmp.84@gmail.com@93da283c-d34f-ab24-ffe0-d8c6b2307087>
2013-07-01 19:27:19 +0000

    Commit inicial

---
+++ modules/validations/trunk/src/test/java/org/mjmayor/validations/ExampleServiceTests.java
@@ -0,0 +1,13 @@
+package org.mjmayor.validations;
+
+import junit.framework.TestCase;
+
+public class ExampleServiceTests extends TestCase {
+
+//	private ExampleService service = new ExampleService();
+//	
+//	public void testReadOnce() throws Exception {
+//		assertEquals("Hello world!", service.getMessage());
+//	}
+}
+
