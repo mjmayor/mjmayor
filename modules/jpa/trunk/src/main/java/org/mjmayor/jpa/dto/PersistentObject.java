79d83286fb620a04672b2b706900f7490ac1068e
mjmp.84@gmail.com <mjmp.84@gmail.com@93da283c-d34f-ab24-ffe0-d8c6b2307087>
2013-08-22 21:04:04 +0000

    Cambiado el tipo del identificador de long a Long para evitar problemas con resultados nulos

--- modules/jpa/trunk/src/main/java/org/mjmayor/jpa/dto/PersistentObject.java
+++ modules/jpa/trunk/src/main/java/org/mjmayor/jpa/dto/PersistentObject.java
@@ -24,13 +24,13 @@ public class PersistentObject implements Serializable {
 	@Id
 	@GenericGenerator(name = PersistentConstants.SERIAL_ID_LABEL, strategy = PersistentConstants.SERIAL_ID_STRATEGY)
 	@GeneratedValue(generator = PersistentConstants.SERIAL_ID_LABEL)
-	private long id;
+	private Long id;
 
-	public long getId() {
+	public Long getId() {
 		return id;
 	}
 
-	public void setId(long id) {
+	public void setId(Long id) {
 		this.id = id;
 	}
 }
