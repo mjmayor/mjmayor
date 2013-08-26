b5eeefe273ce8709ccf4ff5c901db3c66f1a52f2
mjmp.84@gmail.com <mjmp.84@gmail.com@93da283c-d34f-ab24-ffe0-d8c6b2307087>
2013-08-26 22:28:34 +0000

    Eliminado el paquete dao

--- baseproject/trunk/src/main/java/org/mjmayor/baseproject/dao/ProfesorDAO.java
+++
@@ -1,9 +0,0 @@
-package org.mjmayor.baseproject.dao;
-
-import org.mjmayor.baseproject.dto.ProfesorDTO;
-import org.mjmayor.baseproject.form.ProfesorForm;
-import org.mjmayor.jpa.dao.DAO;
-
-public interface ProfesorDAO extends DAO<ProfesorDTO> {
-
-}
