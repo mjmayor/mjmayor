f253b813ee046a0ffabe64b13cb32257ead94085
mjmp.84@gmail.com <mjmp.84@gmail.com@93da283c-d34f-ab24-ffe0-d8c6b2307087>
2013-09-16 20:59:17 +0000

    Cambiada la configuracion

--- baseproject/application/trunk/src/main/java/org/mjmayor/baseproject/config/AppConfig.java
+++ baseproject/application/trunk/src/main/java/org/mjmayor/baseproject/config/AppConfig.java
@@ -1,15 +1,13 @@
 package org.mjmayor.baseproject.config;
 
-import org.mjmayor.jpa.config.JPAConfig;
+import org.mjmayor.jpa.config.aop.JPAConfig;
 import org.mjmayor.persistence.config.PersistenceRepositoryConfig;
-import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
 import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.support.ResourceBundleMessageSource;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
@@ -20,14 +18,6 @@ import org.springframework.web.servlet.view.InternalResourceViewResolver;
 @ComponentScan("org.mjmayor.baseproject")
 @EnableWebMvc
 public class AppConfig extends WebMvcConfigurerAdapter {
-
-	@Bean
-	public PropertyPlaceholderConfigurer getPropertyPlaceholderConfigurer() {
-		PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
-		ppc.setLocation(new ClassPathResource("config/database.properties"));
-		ppc.setIgnoreUnresolvablePlaceholders(true);
-		return ppc;
-	}
 
 	@Bean
 	public InternalResourceViewResolver irResolver() {
