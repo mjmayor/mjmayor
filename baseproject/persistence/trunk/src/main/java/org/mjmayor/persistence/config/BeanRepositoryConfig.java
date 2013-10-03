d731478bb65109ca89c3456a5a7b8fd288b2ba16
mjmp.84@gmail.com <mjmp.84@gmail.com@93da283c-d34f-ab24-ffe0-d8c6b2307087>
2013-10-03 15:57:32 +0000

    Configuracion funcionando (provisional)

--- baseproject/persistence/trunk/src/main/java/org/mjmayor/persistence/config/BeanRepositoryConfig.java
+++ baseproject/persistence/trunk/src/main/java/org/mjmayor/persistence/config/BeanRepositoryConfig.java
@@ -12,7 +12,7 @@ public class BeanRepositoryConfig extends WebMvcConfigurerAdapter {
 	@Bean
 	public PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
 		PropertyPlaceholderConfigurer ppc = new PropertyPlaceholderConfigurer();
-		ppc.setLocation(new ClassPathResource("config/database.properties"));
+		ppc.setLocation(new ClassPathResource("/config/database.properties"));
 		ppc.setIgnoreUnresolvablePlaceholders(true);
 		return ppc;
 	}
--- baseproject/persistence/trunk/src/main/java/org/mjmayor/persistence/config/PersistenceRepositoryConfig.java
+++ baseproject/persistence/trunk/src/main/java/org/mjmayor/persistence/config/PersistenceRepositoryConfig.java
@@ -1,24 +1,33 @@
 package org.mjmayor.persistence.config;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 
-import org.mjmayor.jpa.config.aop.JPAConfig;
-import org.mjmayor.jpa.config.database.AbstractRepositoryConfig;
+import org.mjmayor.jpa.config.database.Dialects;
+import org.mjmayor.jpa.config.database.RepositoryConfig;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.Database;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
-@Import({ BeanRepositoryConfig.class, JPAConfig.class })
-@ComponentScan("org.mjmayor.persistence")
-@PropertySource("classpath:src/main/resources/config/database.properties")
+@Import({ BeanRepositoryConfig.class })
+//@PropertySource("classpath:/config/database.properties")
 @EnableTransactionManagement
-public class PersistenceRepositoryConfig extends AbstractRepositoryConfig {
+public class PersistenceRepositoryConfig extends WebMvcConfigurerAdapter implements RepositoryConfig {
 
 	@Value("${jdbc.driverClassName}")	private String driverClassName;
 	@Value("${jdbc.dialect}")			private String dialect;
@@ -31,9 +40,65 @@ public class PersistenceRepositoryConfig extends AbstractRepositoryConfig {
 	@Value("${hibernate.hbm2ddl.auto}")	private String hibernateHbm2ddlAuto;
 
 	
-	public PersistenceRepositoryConfig() {
-		super();
+	@Autowired
+	private EntityManagerFactory entityManagerFactory;
+
+	private DataSource dataSource;
+
+	private JpaVendorAdapter jpaVendorAdapter;
+
+	private Map<String, Database> databases;
+
+	protected PersistenceRepositoryConfig() {
+		this.databases = buildDatabases();
 	}
+
+	private Map<String, Database> buildDatabases() {
+		Map<String, Database> databases = new HashMap<String, Database>();
+		databases.put(Dialects.MYSQL, Database.MYSQL);
+		return databases;
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Bean
+	public EntityManagerFactory entityManagerFactory() {
+		initDataSource();
+		LocalContainerEntityManagerFactoryBean localContainerEntityManagerFactoryBean = new LocalContainerEntityManagerFactoryBean();
+		localContainerEntityManagerFactoryBean.setDataSource(dataSource);
+		localContainerEntityManagerFactoryBean.setPackagesToScan("org.mjmayor.baseproject");
+		localContainerEntityManagerFactoryBean.setJpaVendorAdapter(jpaVendorAdapter);
+		localContainerEntityManagerFactoryBean.afterPropertiesSet();
+		return localContainerEntityManagerFactoryBean.getNativeEntityManagerFactory();
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	public Database getDatabase(String dialect) {
+		return databases.get(dialect);
+	}
+
+	/**
+	 * {@inheritDoc}
+	 */
+	@Bean
+	public PlatformTransactionManager transactionManager() {
+		initDataSource();
+		JpaTransactionManager transactionManager = new JpaTransactionManager();
+		transactionManager.setEntityManagerFactory(entityManagerFactory);
+		transactionManager.setDataSource(dataSource);
+		return transactionManager;
+	}
+
+	private void initDataSource() {
+		if (dataSource == null) {
+			dataSource = dataSource();
+			jpaVendorAdapter = jpaVendorAdapter();
+		}
+	}
+	
 
 	@Override
 	public DataSource dataSource() {
@@ -47,10 +112,9 @@ public class PersistenceRepositoryConfig extends AbstractRepositoryConfig {
 
 	@Override
 	public JpaVendorAdapter jpaVendorAdapter() {
-		// HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
-		// jpaVendorAdapter.setDatabase(getDatabase(dialect));
-		// jpaVendorAdapter.setShowSql(Boolean.parseBoolean(hibernateShowSql));
-		// return jpaVendorAdapter;
-		return null;
+		HibernateJpaVendorAdapter jpaVendorAdapter = new HibernateJpaVendorAdapter();
+		jpaVendorAdapter.setDatabase(getDatabase(dialect));
+		jpaVendorAdapter.setShowSql(Boolean.parseBoolean(hibernateShowSql));
+		return jpaVendorAdapter;
 	}
 }
