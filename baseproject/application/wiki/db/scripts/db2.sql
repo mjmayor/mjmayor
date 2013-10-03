679959ab8e3f5803b4738df6f0e2c5db79fa94f7
mjmp.84@gmail.com <mjmp.84@gmail.com@93da283c-d34f-ab24-ffe0-d8c6b2307087>
2013-10-03 18:10:16 +0000

    Cambios en el modelo de datos

--- baseproject/application/wiki/db/scripts/db2.sql
+++ baseproject/application/wiki/db/scripts/db2.sql
@@ -11,6 +11,7 @@ create table alumno(
 
 create table profesor(
 	id int primary key,
+	dni varchar(9) unique,
 	nombre varchar(20),
 	apellidos varchar(30)
 );
@@ -18,37 +19,38 @@ create table profesor(
 
 create table asignatura(
 	id int primary key,
+	codigo varchar(10) unique,
 	nombre varchar(30),
 	curso int,
-	creditos double(2,1),
-	id_profesor int,
+	creditos double(2,1)
+	idProfesor int,
 	
-	foreign key(id_profesor)
+	foreign key(idProfesor)
 		references profesor(id)
 );
 
 
 create table matricula(
 	id int primary key,
-	id_alumno int,
-	id_asignatura int,
+	idAlumno int,
+	idAsignatura int,
 	
-	foreign key(id_alumno)
+	foreign key(idAlumno)
 		references alumno(id),
 		
-	foreign key(id_asignatura)
+	foreign key(idAsignatura)
 		references asignatura(id)
 );
 
 
-insert into alumno values (1,"11111111H", "pepe", "perez", "a@a.com");
-insert into alumno values (2,"11111111H", "manuel", "rodriguez", "b@b.com");
-insert into alumno values (3,"11111111H", "pedro", "garcia", "c@c.com");
+insert into alumno values (1, "11111111H", "pepe", "perez", "a@a.com");
+insert into alumno values (2, "22222222J", "manuel", "rodriguez", "b@b.com");
+insert into alumno values (3, "33333333P", "pedro", "garcia", "c@c.com");
 
-insert into profesor values (1,"juan","lopez");
-insert into profesor values (2,"antonio","sanchez");
-insert into profesor values (3,"luis","vazquez");
+insert into profesor values (1, "44444444A", "juan", "lopez");
+insert into profesor values (2, "55555555K", "antonio", "sanchez");
+insert into profesor values (3, "66666666Q", "luis", "vazquez");
 
-insert into asignatura values (1,"asignatura 1",1,9,1);
-insert into asignatura values (2,"asignatura 2",3,7.5,1);
-insert into asignatura values (3,"asignatura 3",5,6,2);
+insert into asignatura values (1, "cod1", "asignatura 1", 1, 9);
+insert into asignatura values (2, "cod2", "asignatura 2", 3, 7.5);
+insert into asignatura values (3, "cod3", "asignatura 3", 5, 6);
