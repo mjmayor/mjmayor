27f2e6db018f81881da0171063dbe3fb2671aab0
mjmp.84@gmail.com <mjmp.84@gmail.com@93da283c-d34f-ab24-ffe0-d8c6b2307087>
2013-10-04 10:44:08 +0000

    Cambios en la configuracion (no crea bien el transactionManager)

--- components/persistence/trunk/src/main/java/org/mjmayor/persistence/service/impl/ProfesorServiceImpl.java
+++ components/persistence/trunk/src/main/java/org/mjmayor/persistence/service/impl/ProfesorServiceImpl.java
@@ -3,13 +3,14 @@ package org.mjmayor.persistence.service.impl;
 import java.util.List;
 
 import org.mjmayor.jpa.assembler.BidirectionalAssembler;
+import org.mjmayor.jpa.dao.DAO;
 import org.mjmayor.jpa.service.impl.ServiceImpl;
 import org.mjmayor.persistence.service.ProfesorService;
 
 public class ProfesorServiceImpl<Profesor, ProfesorDTO> extends ServiceImpl<Profesor, ProfesorDTO> implements ProfesorService<Profesor, ProfesorDTO> {
 
-	public ProfesorServiceImpl(BidirectionalAssembler<Profesor, ProfesorDTO> assembler) {
-		super(assembler);
+	public ProfesorServiceImpl(DAO<Profesor> dao, BidirectionalAssembler<Profesor, ProfesorDTO> assembler) {
+		super(dao, assembler);
 	}
 
 	public List<Profesor> getAlphabeticalList() {
