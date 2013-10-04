b05f3c52807122378764eaf159e8d1d7f7e8cf7a
mjmp.84@gmail.com <mjmp.84@gmail.com@93da283c-d34f-ab24-ffe0-d8c6b2307087>
2013-10-04 10:44:57 +0000

    Cambios en la configuracion (no crea bien el transactionManager)

--- components/persistence/trunk/src/main/java/org/mjmayor/persistence/config/ProfesorPersistenceConfig.java
+++ components/persistence/trunk/src/main/java/org/mjmayor/persistence/config/ProfesorPersistenceConfig.java
@@ -1,5 +1,7 @@
 package org.mjmayor.persistence.config;
 
+import org.mjmayor.jpa.dao.DAO;
+import org.mjmayor.jpa.dao.impl.DAOImpl;
 import org.mjmayor.model.dto.ProfesorDTO;
 import org.mjmayor.model.entity.Profesor;
 import org.mjmayor.persistence.assembler.profesor.ProfesorDTOAssembler;
@@ -13,11 +15,16 @@ public class ProfesorPersistenceConfig {
 
 	@Bean
 	public ProfesorService<Profesor, ProfesorDTO> service() {
-		return new ProfesorServiceImpl<Profesor, ProfesorDTO>(profesorDTOAssembler());
+		return new ProfesorServiceImpl<Profesor, ProfesorDTO>(dao(), profesorDTOAssembler());
 	}
 
 	@Bean
 	public ProfesorDTOAssembler profesorDTOAssembler() {
 		return new ProfesorDTOAssembler();
+	}
+
+	@Bean
+	public DAO<Profesor> dao() {
+		return new DAOImpl<Profesor>(null);
 	}
 }
