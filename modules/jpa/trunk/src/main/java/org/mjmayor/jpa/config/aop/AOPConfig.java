77c0a9a0cba0e973371352efd9ae99167cc34f34
mjmp.84@gmail.com <mjmp.84@gmail.com@93da283c-d34f-ab24-ffe0-d8c6b2307087>
2013-10-04 08:37:41 +0000

    Renombrada la clase JPAConfig

--- modules/jpa/trunk/src/main/java/org/mjmayor/jpa/config/aop/AOPConfig.java
+++ modules/jpa/trunk/src/main/java/org/mjmayor/jpa/config/aop/AOPConfig.java
@@ -7,7 +7,7 @@ import org.springframework.context.annotation.EnableAspectJAutoProxy;
 
 @Configuration
 @EnableAspectJAutoProxy
-public class JPAConfig {
+public class AOPConfig {
 
 	@Bean
 	public JPAInterceptor jpaInterceptor() {
